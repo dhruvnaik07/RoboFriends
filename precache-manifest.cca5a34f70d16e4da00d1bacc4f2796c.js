3845d7d4f23aaa10b08913bfde8d5136367c9dd5
dhruvnaik07 <dhruv3456@gmail.com>
2019-10-06 16:57:30 -0400

    Updates

---
+++ precache-manifest.cca5a34f70d16e4da00d1bacc4f2796c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f609a40b6a3cdddb2d1df6e455e37d",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "200c6d7eecdb7727964e",
+    "url": "/RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b53a9a11233f4781a2c3",
+    "url": "/RoboFriends/static/css/main.4d4690c6.chunk.css"
+  },
+  {
+    "revision": "200c6d7eecdb7727964e",
+    "url": "/RoboFriends/static/js/2.02ef720e.chunk.js"
+  },
+  {
+    "revision": "b53a9a11233f4781a2c3",
+    "url": "/RoboFriends/static/js/main.a219e159.chunk.js"
+  },
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
